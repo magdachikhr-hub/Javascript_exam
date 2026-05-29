368ea9e47fe934cc0869b736019a4f5c1001a808
magda chikhradz <magda292005@gmail.com>
2026-05-29 17:14:47 +0400

    task 1 done

---
+++ TASK_1/main.js
@@ -0,0 +1,12 @@
+//1.1 1.2 1.3 1.4
+
+const num1 = 50;
+const num2 = 20;
+
+function multiplyNumbers(num1, num2) {
+  const result = num1 * num2;
+  console.log(result);
+  return result;
+}
+
+multiplyNumbers(num1, num2);
